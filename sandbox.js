6f7d0970155c772ab8409a6c76dba23bf136321c
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-09-07 09:23:53 +0200

    defined first type

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,10 @@
+//"tsc sandbox.ts -w" command is very annoying don't use it, compile when you want to manually
 var test = "testing";
 console.log(test);
 var inputs = document.querySelectorAll('input');
 inputs.forEach(function (input) { return console.log(input); });
+//prevent errors by defining a type in development, before compiling
+var circ = function (dia) {
+    return dia * Math.PI;
+};
+console.log(circ(21));
--- sandbox.ts
+++ sandbox.ts
@@ -1,6 +1,14 @@
+//"tsc sandbox.ts -w" command is very annoying don't use it, compile when you want to manually
 const test = "testing"
 console.log(test)
 
 const inputs = document.querySelectorAll('input')
 
 inputs.forEach(input =>  console.log(input))
+
+//prevent errors by defining a type in development, before compiling
+const circ = (dia: number) => {
+  return dia * Math.PI
+}
+
+console.log(circ(21))
