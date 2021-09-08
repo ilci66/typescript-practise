6e8ae64450f5b4de3f84064365dd3151721207a2
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-09-08 14:41:44 +0200

    working on rendering html template

--- src/app.ts
+++ src/app.ts
@@ -169,6 +169,36 @@ import { HasFormatter } from './interfaces/HasFormatter.js';
 // docs.push(docOne);
 // docs.push(docTwo);
 
+// const form = document.querySelector('.new-item-form') as HTMLFormElement
+
+// const type = document.querySelector('#type') as HTMLSelectElement;
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// form.addEventListener('submit', (e: Event) => {
+//   e.preventDefault();
+
+//   let doc: HasFormatter;
+//   if(type.value === "invoice"){
+//     doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+//   }else {
+//     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+//   }
+
+//   // console.log(
+//   //   type.value,
+//   //   tofrom.value,
+//   //   details.value,
+//   //   amount.valueAsNumber
+//   //   )
+//   // instead of all this now I can just use console.log(doc)
+//   console.log(doc)
+// })
+
+
+// // >>>>> RENDERING AN HTML TEMPLATE
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement
 
 const type = document.querySelector('#type') as HTMLSelectElement;
@@ -186,12 +216,5 @@ form.addEventListener('submit', (e: Event) => {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
   }
 
-  // console.log(
-  //   type.value,
-  //   tofrom.value,
-  //   details.value,
-  //   amount.valueAsNumber
-  //   )
-  // instead of all this now I can just use console.log(doc)
   console.log(doc)
 })
---
+++ src/classes/ListTemplate.ts
@@ -0,0 +1,24 @@
+import { HasFormatter } from "../interfaces/HasFormatter.js";
+
+export class ListTemplate {
+  // gonna use the shorthand method to keep it cleaner
+  constructor(private container: HTMLUListElement){}
+
+  // now need a render method to create 'li elements
+  // that will accept the arguments: invoice or payment, a heading, and a position
+  // that will create the necessary templates (li, h4, p)
+  // that will add the 'li' template to the start or to the end of the list
+
+  // now the type need to include both payment and invoice so it can't only be one of them
+  // as both of them have the have the HasFormatter interface implemented I can use that for type here
+  // the pos type is regular union type don't get confused
+  render(item: HasFormatter, heading: string, pos: 'start' | 'end'){
+    //now to create the elements
+    const li = document.createElement('li');
+    
+    const h4 = document.createElement('h4');
+    h4.innerText = heading;
+    li.append(h4);
+    //to be continued
+  }
+}
